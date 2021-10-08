a61132e9ed55d0fbb8e0c4ade0beb2ad607dbc2c
moyarka <mahyar.karimi2079@ut.ac.ir>
2021-10-09 01:37:59 +0330

    initial tests

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerTest.java
@@ -2,6 +2,19 @@ package org.springframework.samples.petclinic.owner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class OwnerTest {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 
+class OwnerTest {
+    private Owner owner = new Owner();
+    @Test
+    void testGetAddress() {
+        assertEquals(null, owner.getAddress());
+    }
+
+    @Test
+    void testSetAddress() {
+        owner.setAddress("karaj");
+        assertEquals("karaj", owner.getAddress());
+    }
 }

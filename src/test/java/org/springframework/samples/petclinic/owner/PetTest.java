977989a898d8fe518a4c536984d24b1495f7a04c
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-10-11 13:34:46 +0200

    Added testGetVisits for Pet class with Theories

--- src/test/java/org/springframework/samples/petclinic/owner/PetTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetTest.java
@@ -1,11 +1,68 @@
 package org.springframework.samples.petclinic.owner;
 
-import org.junit.jupiter.api.Test;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 import org.springframework.samples.petclinic.visit.Visit;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.time.LocalDate;
+import java.util.*;
 
-class PetTest {
+@RunWith(Theories.class)
+public class PetTest {
+	
+	@DataPoint
+	public static List<Visit> visits1() {
+		List<Visit> vlist = new ArrayList<>();
+		Visit v1 = new Visit();
+		Visit v2 = new Visit();
+		Visit v3 = new Visit();
 
-	// todo
+		v1.setDate(LocalDate.of(2000, 1, 21));
+		vlist.add(v1);
+		v2.setDate(LocalDate.of(2001, 4, 17));
+		vlist.add(v2);
+		v3.setDate(LocalDate.of(1999, 4, 19));
+		vlist.add(v3);
+
+		return vlist;
+	}
+
+	@DataPoint
+	public static List<Visit> visits2() {
+		List<Visit> vlist = new ArrayList<>();
+		Visit v1 = new Visit();
+		Visit v2 = new Visit();
+		Visit v3 = new Visit();
+
+		v1.setDate(LocalDate.of(2000, 10, 7));
+		vlist.add(v1);
+		v2.setDate(LocalDate.of(2000, 11, 19));
+		vlist.add(v2);
+		v3.setDate(LocalDate.of(1999, 4, 19));
+		vlist.add(v3);
+
+		return vlist;
+	}
+
+	@Theory
+	public void testGetVisits(List<Visit> visitList) {
+		Pet pet = new Pet();
+		for (Visit visit: visitList)
+			pet.addVisit(visit);
+
+		List<Visit> expected = visitList;
+		Collections.sort(expected, new Comparator<Visit>() {
+			@Override
+			public int compare(Visit v1, Visit v2) {
+			  return v2.getDate().compareTo(v1.getDate());
+			}
+		  });
+	
+		System.out.println(expected);
+		System.out.println(pet.getVisits());
+		assertEquals(expected, pet.getVisits());
+
+	}
 }

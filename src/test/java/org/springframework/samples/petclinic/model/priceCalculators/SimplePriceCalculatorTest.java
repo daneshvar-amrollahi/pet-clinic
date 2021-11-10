7908ce7ea2d54f87d37600549727c5fda75da71d
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-11-10 20:41:01 +0100

    Added SimplePriceCalculator Tests

--- src/test/java/org/springframework/samples/petclinic/model/priceCalculators/SimplePriceCalculatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/model/priceCalculators/SimplePriceCalculatorTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.samples.petclinic.model.Pet;
 import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.model.UserType;
 
 public class SimplePriceCalculatorTest {
     private SimplePriceCalculator priceCalculator;
@@ -19,15 +20,48 @@ public class SimplePriceCalculatorTest {
     }
 
     @Test
-    public void testCalculatePrice() {
+    public void testCalcPrice1() {
         List<Pet> pets = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             Pet pet = new Pet();
-            pet.setType(new PetType());
+
+            PetType petType = new PetType();
+			if (i % 2 == 1)
+				petType.setRare(true);
+			else
+				petType.setRare(false);
+
+            pet.setType(petType);
+
             pets.add(pet);
             System.out.println(pets.get(i).getType().getRare());
         }
 
-        assertTrue(1 == 1);
+        UserType userType = UserType.NEW;
+        double expected = priceCalculator.calcPrice(pets, 0, 1, userType);
+    	assertTrue(expected == 3.04);
     }
+
+    @Test
+	public void testCalcPrice2() {
+		List<Pet> pets = new ArrayList<>();
+		for (int i = 0; i < 3; i++) {
+			Pet pet = new Pet();
+
+			PetType petType = new PetType();
+			if (i % 2 == 1)
+				petType.setRare(true);
+			else
+				petType.setRare(false);
+
+			pet.setType(petType);
+
+			pets.add(pet);
+			System.out.println(pets.get(i).getType().getRare());
+		}
+
+		UserType userType = UserType.SILVER;
+		double actual = priceCalculator.calcPrice(pets, 0, 1, userType);
+		assertTrue(actual == 3.2);
+	}
 }

ce0ae519c48cc0bc8595bb3b3b245939ea20bb5c
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-12-08 16:37:13 +0100

    Cleaned up code

--- src/test/java/org/springframework/samples/petclinic/owner/PetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetControllerTests.java
@@ -1,38 +1,21 @@
 package org.springframework.samples.petclinic.owner;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import org.aspectj.lang.annotation.Before;
 import org.assertj.core.util.Lists;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.samples.petclinic.utility.PetTimedCache;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-@WebMvcTest(value = PetController.class,
-	includeFilters = {
-		@ComponentScan.Filter(value = PetTypeFormatter.class, type = FilterType.ASSIGNABLE_TYPE),
-		@ComponentScan.Filter(value = PetService.class, type = FilterType.ASSIGNABLE_TYPE),
-		@ComponentScan.Filter(value = LoggerConfig.class, type = FilterType.ASSIGNABLE_TYPE),
-		@ComponentScan.Filter(value = PetTimedCache.class, type = FilterType.ASSIGNABLE_TYPE),
-	}
-	)
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
 class PetControllerTests {
-	private final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
 	@Autowired
 	private MockMvc mockMvc;
 
@@ -45,32 +28,34 @@ class PetControllerTests {
 	private Owner owner;
 	private Pet pet;
 
-	@BeforeEach
-	void setup() {
-		PetType dog = new PetType();
-		dog.setId(10);
-		dog.setName("funfe");
-		pet = new Pet();
-		pet.setId(100);
-		owner = new Owner();
-		owner.setId(1);
-		given(this.pets.findPetTypes()).willReturn(Lists.newArrayList(dog));
-		given(this.owners.findById(1)).willReturn(owner);
-		given(this.pets.findById(100)).willReturn(pet);
-	}
 
 	@Test
 	void testInitCreationForm() throws Exception {
+		owner = new Owner();
+		owner.setId(1);
+		when(this.owners.findById(1)).thenReturn(owner);
 		ResultActions resultActions = mockMvc.perform(get("/owners/1/pets/new"));
 		resultActions.andExpect(status().isOk());
 	}
 
 	@Test
 	void testProcessCreationForm() throws Exception {
+		PetType dog = new PetType();
+		dog.setId(10);
+		dog.setName("jack");
+		pet = new Pet();
+		pet.setId(100);
+		owner = new Owner();
+		owner.setId(1);
+		when(this.pets.findPetTypes()).thenReturn(Lists.newArrayList(dog));
+		when(this.owners.findById(1)).thenReturn(owner);
+		when(this.pets.findById(100)).thenReturn(pet);
+
+
 		ResultActions resultActions =
 			mockMvc.perform(post("/owners/1/pets/new")
 				.param("name", "Alfred")
-				.param("type", "funfe")
+				.param("type", "jack")
 				.param("birthDate", "2019-11-18")
 			);
 		resultActions.andExpect(status().is3xxRedirection());
@@ -78,6 +63,10 @@ class PetControllerTests {
 
 	@Test
 	void testInitUpdateForm() throws Exception {
+		pet = new Pet();
+	 	pet.setId(100);
+		when(this.pets.findById(100)).thenReturn(pet);
+
 		ResultActions resultActions = mockMvc.perform(get("/owners/1/pets/100/edit"));
 		resultActions.andExpect(status().isOk());
 		resultActions.andExpect(view().name("pets/createOrUpdatePetForm"));
@@ -86,10 +75,21 @@ class PetControllerTests {
 
 	@Test
 	void testProcessUpdateForm() throws Exception {
+		PetType dog = new PetType();
+		dog.setId(10);
+		dog.setName("jack");
+		pet = new Pet();
+		pet.setId(100);
+		owner = new Owner();
+		owner.setId(1);
+		when(this.pets.findPetTypes()).thenReturn(Lists.newArrayList(dog));
+		when(this.owners.findById(1)).thenReturn(owner);
+		when(this.pets.findById(100)).thenReturn(pet);
+
 		ResultActions resultActions =
 			mockMvc.perform(post("/owners/1/pets/100/edit")
 				.param("name", "Alfred")
-				.param("type", "funfe")
+				.param("type", "jack")
 				.param("birthDate", "2019-11-18")
 			);
 

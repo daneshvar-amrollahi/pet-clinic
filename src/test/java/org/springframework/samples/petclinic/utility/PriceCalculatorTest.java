95dad88d3f2caedbf4487baa48192377b827d706
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-12-18 23:46:25 +0100

    Added PriceCalculator test

--- src/test/java/org/springframework/samples/petclinic/utility/PriceCalculatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/utility/PriceCalculatorTest.java
@@ -1,6 +1,5 @@
 package org.springframework.samples.petclinic.utility;
 
-import jdk.vm.ci.meta.Local;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.samples.petclinic.owner.Pet;
@@ -8,11 +7,12 @@ import org.springframework.samples.petclinic.visit.Visit;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PetCalculatorTest {
+public class PriceCalculatorTest {
 
 	private PriceCalculator priceCalculator;
 	private Visit visit1, visit2;
@@ -27,39 +27,32 @@ public class PetCalculatorTest {
 	}
 
 	@Test
-	public void testPriceCalc1() {
+	public void testPriceCalc() {
+		List<Visit> visits = new ArrayList<>();
 		List<Pet> pets = new ArrayList<>();
-		for (int i = 0 ; i < 10 ; i++)
-		{
+
+		for (int i = 98; i < 103; i++) {
+			Visit visit = new Visit();
+			visit.setDate(LocalDate.now().minusDays(i));
+			visits.add(visit);
+		}
+
+		for (int i = 0; i < 10; i++) {
 			Pet pet = new Pet();
-			if (i % 3 == 0)
-			{
-				LocalDate birthDate = LocalDate.now();
-				pet.setBirthDate(birthDate);
-
-				for (int j = 0 ; j < 3 ; j++)
-					pet.addVisit(visit1);
-			}
-			if (i % 3 == 1)
-			{
-				LocalDate birthDate = LocalDate.now().minusYears(3);
-				pet.setBirthDate(birthDate);
-
-				for (int j = 0 ; j < 3 ; j++)
-					pet.addVisit(visit2);
-			}
-			if (i % 3 == 2)
-			{
-				LocalDate birthDate = LocalDate.now();
-				pet.setBirthDate(birthDate);
-
-				for (int j = 0 ; j < 3 ; j++)
-					pet.addVisit(visit1);
-			}
+			pet.setBirthDate(LocalDate.now().minusYears(i % 5));
 			pets.add(pet);
 		}
 
-		double expected = 3633.88;
-		assertEquals(expected, priceCalculator.calcPrice(pets, 5, 12));
+		for (int i = 0 ; i < 10 ; i++)
+		{
+			Pet pet = pets.get(i);
+			int n = 5 - i % 4;
+			for (int j = 0; j < n; j++) {
+				pet.addVisit(visits.get(j));
+			}
+		}
+
+		double expected = 52318.6;
+		assertEquals(expected, PriceCalculator.calcPrice(pets, 5, 12));
 	}
 }

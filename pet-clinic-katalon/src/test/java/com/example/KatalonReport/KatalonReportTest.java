9efb08236567ba1bf974e5bc75b8e008b4d4f996
moyarka <mahyar.karimi2079@gmail.com>
2021-12-30 00:36:49 +0330

    Modified Katalon test

---
+++ pet-clinic-katalon/src/test/java/com/example/KatalonReport/KatalonReportTest.java
@@ -0,0 +1,107 @@
+package com.example.KatalonReport;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class KatalonReportTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "./chromedriver");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testKatalonReport() throws Exception {
+    //ERROR: Caught exception [unknown command []]
+    driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[2]/a/span[2]")).click();
+    driver.findElement(By.linkText("Add Owner")).click();
+    driver.findElement(By.id("firstName")).click();
+    driver.findElement(By.id("firstName")).clear();
+    driver.findElement(By.id("firstName")).sendKeys("Mahyar");
+    driver.findElement(By.id("lastName")).clear();
+    driver.findElement(By.id("lastName")).sendKeys("Karimi");
+    driver.findElement(By.id("address")).clear();
+    driver.findElement(By.id("address")).sendKeys("Pamchal St.");
+    driver.findElement(By.id("city")).clear();
+    driver.findElement(By.id("city")).sendKeys("Karaj");
+    driver.findElement(By.id("telephone")).clear();
+    driver.findElement(By.id("telephone")).sendKeys("1234567890");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    driver.findElement(By.xpath("//a[contains(text(),'Add\n      New Pet')]")).click();
+    driver.findElement(By.id("name")).click();
+    driver.findElement(By.id("name")).clear();
+    driver.findElement(By.id("name")).sendKeys("Pat");
+    driver.findElement(By.id("birthDate")).clear();
+    driver.findElement(By.id("birthDate")).sendKeys("2021-12-29");
+    new Select(driver.findElement(By.id("type"))).selectByVisibleText("dog");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    driver.findElement(By.xpath("//a[contains(text(),'Add\n                  Visit')]")).click();
+    driver.findElement(By.id("date")).click();
+    driver.findElement(By.id("date")).clear();
+    driver.findElement(By.id("date")).sendKeys("2021-12-30");
+    driver.findElement(By.id("description")).clear();
+    driver.findElement(By.id("description")).sendKeys("First visit");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[3]/a/span[2]")).click();
+    driver.findElement(By.xpath("//table[@id='vets']/tbody")).click();
+    driver.findElement(By.xpath("//table[@id='vets']/tbody/tr[6]/td[2]/span")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
